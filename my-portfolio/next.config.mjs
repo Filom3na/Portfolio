ce6bdf8b745a31f83aa4ceb9e6b7f35bfb272d38
Filom3na <filomenamurgo@yahoo.com>
2024-09-12 05:34:10 +0200

    changess

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
